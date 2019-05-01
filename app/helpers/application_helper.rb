a2735f82fa94aa3b724dc415877c24dbf951de5d
Ivan Szebenszki <szib77@gmail.com>
2019-05-01 09:07:07 +0100

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def format_appointment_datetime(datetime)
+    # January 12, 2016 at 3:00
+    datetime.strftime('%B %d, %Y at %k:%M')
+  end
 end
--- app/helpers/doctors_helper.rb
+++ app/helpers/doctors_helper.rb
@@ -1,2 +1,5 @@
 module DoctorsHelper
+  def patient_name_with_appointment_time(appointment)
+    [appointment.patient.name, '-', format_appointment_datetime(appointment.appointment_datetime)].join(' ')
+  end
 end
